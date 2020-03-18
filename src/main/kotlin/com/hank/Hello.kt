6032d7182f9d8a62d343dcbe469ec3d47b368ac8
Strong <qian0934@gmail.com>
2020-03-18 11:34:27 +0800

    Initial commit

---
+++ src/main/kotlin/com/hank/Hello.kt
@@ -0,0 +1,14 @@
+package com.hank
+
+fun main() {
+//    println("Hello World")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
